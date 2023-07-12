56be42717393e6a7102999c0206700dab7630ccb
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-07-12 21:57:49 +0530

    pooja shop

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        src="https://gamma.app/embed/ne8a5miun8gx8gm"
+        style={{ width: '100%', height: '100vh' }}
+        allow="fullscreen"
+        title="Welcome to Our Ambay Pooja Shop!"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
